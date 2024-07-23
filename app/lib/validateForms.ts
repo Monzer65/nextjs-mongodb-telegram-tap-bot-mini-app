93c102d24a8957e131db6e1d49cae6f97faeca94
monzer <danesh.monzer@gmail.com>
2024-07-23 22:05:45 +0330

    auth deleted

--- app/lib/validateForms.ts
+++
@@ -1,9 +0,0 @@
-export function validateEmail(email: string) {
-  var re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
-
-export function validatePassword(password: string) {
-  const pattern = /^.{6,}$/;
-  return pattern.test(password);
-}
