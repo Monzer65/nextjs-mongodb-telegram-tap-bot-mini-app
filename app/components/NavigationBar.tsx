ad07b75e1269e5099321c76e9c6a61fda6a01fc5
monzer <danesh.monzer@gmail.com>
2024-08-12 08:09:05 +0330

    solve the z-index of nav links

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -14,7 +14,7 @@ function NavLinks() {
   const pathname = usePathname();
 
   return (
-    <nav className='sticky bottom-0 left-0 right-0 border'>
+    <nav className='sticky bottom-0 left-0 right-0 z-50'>
       <ul className='flex justify-between text-xs bg-gray-700 text-white'>
         <li className={`${pathname === "/bot" ? "bg-gray-900" : ""}`}>
           <Link
