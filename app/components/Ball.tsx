f649ea471ee95755b0946a2874d149e8bab976c6
monzer <danesh.monzer@gmail.com>
2024-08-20 13:23:09 +0330

    build error removed (1)

--- app/components/Ball.tsx
+++ app/components/Ball.tsx
@@ -1,21 +1,21 @@
-"use client";
-import { useAtom } from "jotai";
-import Image from "next/image";
-import { coinsAtom, incrementByAtom } from "../lib/atoms";
-const Ball = () => {
-  const [balance, setBalance] = useAtom(coinsAtom);
-  const [perTap] = useAtom(incrementByAtom);
+// "use client";
+// import { useAtom } from "jotai";
+// import Image from "next/image";
+// import { coinsAtom, incrementByAtom } from "../lib/atoms";
+// const Ball = () => {
+//   const [balance, setBalance] = useAtom(coinsAtom);
+//   const [perTap] = useAtom(incrementByAtom);
 
-  function handleBallTap() {
-    return setBalance((prev) => prev + perTap);
-  }
-  return (
-    <button onClick={handleBallTap} className='m-auto border text-white'>
-      <p>per tap: {perTap}</p>
-      <p>balance: {balance}</p>
-      <Image src='/ball.svg' alt='ball svg' width={150} height={150} />
-    </button>
-  );
-};
+//   function handleBallTap() {
+//     return setBalance((prev) => prev + perTap);
+//   }
+//   return (
+//     <button onClick={handleBallTap} className='m-auto border text-white'>
+//       <p>per tap: {perTap}</p>
+//       <p>balance: {balance}</p>
+//       <Image src='/ball.svg' alt='ball svg' width={150} height={150} />
+//     </button>
+//   );
+// };
 
-export default Ball;
+// export default Ball;
